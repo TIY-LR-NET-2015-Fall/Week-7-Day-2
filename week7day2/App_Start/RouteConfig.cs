4c71382c0eef7a1e6313e4634cd6aed251f4ed9e
Cason Barnhill <casonbarnhill@yahoo.com>
2015-12-02 16:32:39 -0600

    added vanity urls

--- week7day2/App_Start/RouteConfig.cs
+++ week7day2/App_Start/RouteConfig.cs
@@ -14,6 +14,18 @@ namespace week7day2
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+               name: "WallMessage",
+               url: "wall/{name}/{message}",
+               defaults: new { controller = "classmates", action = "PostWallMessage", name = "", message = ""}
+           );
+
+            routes.MapRoute(
+               name: "StudentList",
+               url: "students",
+               defaults: new { controller = "classmates", action = "List", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
