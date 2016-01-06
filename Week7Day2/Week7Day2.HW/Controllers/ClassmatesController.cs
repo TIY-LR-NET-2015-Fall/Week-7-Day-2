e9777c2bd455219bbbcc2a7ac35415145f7b7824
Josh Hill <joshuahill84@gmail.com>
2016-01-06 11:40:25 -0600

    added classmate list view

---
+++ Week7Day2/Week7Day2.HW/Controllers/ClassmatesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Week7Day2.HW.Models;
+
+namespace Week7Day2.HW.Controllers
+{
+    public class ClassmatesController : Controller
+    {
+        // GET: Classmates
+        public ActionResult List()
+        {
+            var model = new List<ClassmateVM>
+            {
+                new ClassmateVM() {HairColor = "brown", Height = 80, Name = "Daniel Pollock"},
+                new ClassmateVM() {HairColor = "red", Height = 72, Name = "Baxley"},
+                new ClassmateVM() {HairColor = "green", Height = 64, Name = "Justin"},
+                new ClassmateVM() {HairColor = "blue", Height = 72, Name = "Cason"}
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Week7Day2/Week7Day2.HW/Models/ClassmateVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week7Day2.HW.Models
+{
+    public class ClassmateVM
+    {
+        public string HairColor { get; set; }
+        public int Height { get; set; }
+        public string Name { get; set; }
+
+    }
+}
